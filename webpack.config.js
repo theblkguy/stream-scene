8edd0dfefc24229dafec6c5825ea3cab3b3348dd
theblkguy <allblk13@gmail.com>
2025-07-28 18:16:02 -0500

    Fix/ Removed conflicting react dependencies, updated HTML configuration in webpack, "Quiet on the set!" rendering again

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -12,12 +13,12 @@ export default {
     publicPath: '/',
   },
   resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
+    extensions: ['.ts', '.js'],
   },
   module: {
     rules: [
       {
-        test: /\.(ts|tsx)$/,
+        test: /\.ts$/,
         use: 'ts-loader',
         exclude: /node_modules/,
       },
@@ -27,12 +28,18 @@ export default {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './client/src/index.html',
+      filename: 'index.html',
+    }),
+  ],
   devtool: 'source-map',
   devServer: {
     static: {
       directory: path.resolve(__dirname, 'dist/public'),
     },
-    port: 3000,
+    port: 3001,
     hot: true,
     open: true,
     historyApiFallback: true,
