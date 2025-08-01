5a4e7cb04fb5f1d0791ff0199b5d6738e78bf485
theblkguy <allblk13@gmail.com>
2025-08-01 10:27:12 -0500

    Add/ Stroke size, eraser, pen color functionality to canvas

--- client/components/ProjectHub/ProjectHubCanvas.tsx
+++ client/components/ProjectHub/ProjectHubCanvas.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { ReactSketchCanvas } from "react-sketch-canvas";
 
 const canvasStyles = {
@@ -7,15 +7,52 @@ const canvasStyles = {
   marginBottom: "20px",
 };
 
+
 const ProjectHubCanvas: React.FC = () => {
+  const [strokeColor, setStrokeColor] = useState<string>("#000000");
+  const [strokeWidth, setStrokeWidth] = useState<number>(2);
+  const [eraserMode, setEraserMode] = useState<boolean>(false);
+
   return (
-    <ReactSketchCanvas
-      style={canvasStyles}
-      width="600px"
-      height="400px"
-      strokeWidth={2}
-      strokeColor="black"
-    />
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
+      <div style={{ display: "flex", gap: "16px", marginBottom: "12px" }}>
+        <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          Color:
+          <input
+            type="color"
+            value={strokeColor}
+            onChange={e => setStrokeColor(e.target.value)}
+            disabled={eraserMode}
+          />
+        </label>
+        <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          Size:
+          <input
+            type="range"
+            min={1}
+            max={20}
+            value={strokeWidth}
+            onChange={e => setStrokeWidth(Number(e.target.value))}
+          />
+          <span>{strokeWidth}px</span>
+        </label>
+        <label style={{ display: "flex", alignItems: "center", gap: "4px" }}>
+          <input
+            type="checkbox"
+            checked={eraserMode}
+            onChange={e => setEraserMode(e.target.checked)}
+          />
+          Eraser
+        </label>
+      </div>
+      <ReactSketchCanvas
+        style={canvasStyles}
+        width="600px"
+        height="400px"
+        strokeWidth={strokeWidth}
+        strokeColor={eraserMode ? "#fff" : strokeColor}
+      />
+    </div>
   );
 };
 
