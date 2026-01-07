4b6dd5008ece2e0a16fd0d9233cd8c4a3cace288
theblkguy <allblk13@gmail.com>
2026-01-07 10:50:18 -0600

    Update server.ts

--- server/server.ts
+++ server/server.ts
@@ -3,7 +3,6 @@ import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import fs from 'fs';
-import sequelize from './src/db/db.js';
 
 // ES module equivalent of __dirname
 const __filename = fileURLToPath(import.meta.url);
